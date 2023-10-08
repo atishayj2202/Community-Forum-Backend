1b06882107cd079c5bfae7f403ca7381b8501abf
ATISHAY JAIN <atishayj2202@gmail.com>
2023-10-09 03:37:04 +0530

    Added User Handler

--- Test/__init__.py
+++ Test/__init__.py
@@ -1,6 +1,5 @@
 from src.client.db import CockroachDBClient
-from src.db.main import get_All_Post
-
+from src.db.articleHandler import add_Post
 eng = CockroachDBClient()
 
-print(get_All_Post(eng.get_session()))
+print(add_Post(eng.get_session(), "Hello Tony Lets make Some power", "I'm Asshole", "Apple", "79f4bba1-c7b4-4d4a-a011-2a6b66f2b332"))
