28f060d5ad66bfcd67bdec21f041889963c13421
ATISHAY JAIN <atishayj2202@gmail.com>
2023-10-14 00:30:56 +0530

    Updated Error

--- src/db/userHandler.py
+++ src/db/userHandler.py
@@ -12,7 +12,16 @@ def create_user(Session, author_name, uid):
         session.commit()
         return {"Status": "Success", "Data": str(id)}
     except:
-        return {"Status": "Error", "Data": "Unexpected Error"}
+        try:
+            session = Session()
+            result = session.execute(statement=text("SELECT * FROM authors WHERE uid = :el1;"),
+                                     params={"el1": uid}).fetchall()
+            if len(result) < 1:
+                return {"Status": "Error", "Data": "Unexpected Error1"}
+            return {"Status": "Success",
+                    "Data": str(result[0][2])}
+        except:
+            return {"Status": "Error", "Data": "Unexpected Error2"}
 
 
 def get_user(Session, id):
@@ -22,6 +31,7 @@ def get_user(Session, id):
                                  params={"el1": id}).fetchall()
         if len(result) < 1:
             return {"Status": "Not Success", "Data": "Not Found"}
-        return {"Status": "Success", "Data": {"id" : str(result[0][0]),"name" : str(result[0][1]), "uid" :str(result[0][2])}}
+        return {"Status": "Success",
+                "Data": {"id": str(result[0][0]), "name": str(result[0][1]), "uid": str(result[0][2])}}
     except:
         return {"Status": "Error", "Data": "Unexpected Error"}
